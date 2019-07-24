9d2910cf2dc8c6d03ef9c31a7d66703af4580177
zyd <123>
2019-07-24 14:40:49 +0800

    this is a new test

--- app/src/main/java/com/example/evaluation_system/base/model/Answer.java
+++ app/src/main/java/com/example/evaluation_system/base/model/Answer.java
@@ -1,6 +1,6 @@
 package com.example.evaluation_system.base.model;
 
-public class Answer {//this this a new test
+public class Answer {//222local change sth
     //i add a line;
     private Integer studentid;
 
